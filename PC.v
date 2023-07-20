cfba0b65a6e6d29d18836445f2f300a519fe25f3
PrecureKamiyu <92973191+PrecureKamiyu@users.noreply.github.com>
2023-07-21 06:27:28 +0800

    Update PC.v
    change pc's type

--- PC.v
+++ PC.v
@@ -5,16 +5,15 @@ module PC (
     input wire         clk,
     input wire         data_hazard,
     input wire 	       control_hazard,
-    output wire [31:0] pc
+    output reg  [31:0] pc
     );
-    reg [31:0]         curr_adr;
 
     always @(posedge clk or negedge rst) begin
         if (rst == 1) begin
     `ifdef RUN_TRACE
-            curr_adr <= -8;
+            pc <= -8;
     `else
-            curr_adr <= 0;
+            pc <= 0;
     `endif
         end
         else if (control_hazard)
@@ -22,8 +21,6 @@ module PC (
         else if (data_hazard)
             pc <= pc;
         else
-            curr_adr <= npc;
+            pc <= npc;
     end
-
-    assign pc = curr_adr;
 endmodule
